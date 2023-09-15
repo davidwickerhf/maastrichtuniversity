19f00a33740dcce5934859392d2dde63fa84642c
David Wicker <davidwickerhf@gmail.com>
2023-09-15 12:21:53 +0200

    Assignment 6.2 (not passing tests)

--- BCS1120 Procedural Programming/assignments/assignment-6/ArrayAssignment.java
+++ BCS1120 Procedural Programming/assignments/assignment-6/ArrayAssignment.java
@@ -1,13 +1,195 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class ArrayAssignment {
 
     public static void main(String[] args) {
         int[][] testArray = new int[][]{{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
-        diagonalPrint(testArray);
+        //diagonalPrint(testArray);
+        //System.out.println();
+
+        int[] a = new int[]{0,2,0,2,0,2,2,1,1,0};
+        //System.out.println(allNumsWithin(a, 3)); // Expect 4
+
+        int[] b = new int[]{0,1,1,4,0,2,0,1,0,2};
+        //System.out.println(allNumsWithin(b, 5)); // Expect 0
+
+        int[] c = new int[]{1, 2, 2, 2, 2, 2, 2, 0};
+        System.out.println(allNumsWithin(c, 3)); // Expect 8??? 2
+        // 1, 2, 2, 2, 2, 2, 2, 0
     }
 
     public static int allNumsWithin(int[] A, int k) {
+
+        int direction = 0; // 0 for descending, 1 for ascending
+        ArrayList<ArrayList<Integer>> sequences = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> sequence = new ArrayList<Integer>();
+
+        // Generate list of possible ordered sequences
+        for (int i = 0; i < A.length; i++) {
+            // If first loop
+            if (i == 0) {
+                // Initialize sequence
+                sequence.add(A[i]);
+                
+                // Initialize direction
+                direction = detectDirection(A, i);
+            } else {
+
+                // Process sequence elements
+
+                // Check direction match and "consecutiveness"
+                boolean directionValid = false;
+                boolean sequenceValid = false;
+                if (A[i] >= A[i-1] && direction == 1) {
+                    // Direction is ascending, check sequence
+                    directionValid = true;
+                    if (A[i] == A[i-1] + 1 || A[i] == A[i-1]) {
+                        // Sequence is correct
+                        sequenceValid = true;
+                        
+                    }
+                    
+                } else if (A[i] <= A[i-1] && direction == 0){
+                    directionValid = true;
+                    // Direction is descending, check sequence
+                    if (A[i] == A[i-1] - 1 || A[i] == A[i-1]) {
+                        // Sequence is correct
+                        sequenceValid = true;
+                    }
+                }
+
+                if (sequenceValid && directionValid) {
+                    sequence.add(A[i]);
+
+                    // Add sequence to sequences array if the loop ended
+                    if (i == A.length-1 && sequence.size() > 1) {
+                        sequences.add(sequence);
+                    }
+                } else {
+
+                    // Reset sequence (Direction change or skip in sequence)
+                    
+                    // Check if previous sequence is valid
+                    if (sequence.size() > 1) {
+                        // Check if sequence contains all numbers between 0 and k-1
+                        int[] correctSequence = IntStream.range(0, k).toArray();
+
+                        boolean orderedSequence = true;
+                        for (int el : correctSequence) {
+                            if (!sequence.contains(el)) {
+                                orderedSequence = false;
+                            }
+                        }
+
+                        if (orderedSequence) {
+                            // Sequence is valid, store
+                            sequences.add(sequence);
+                        } 
+                    }
+
+                    // Reset variables
+                    sequence.clear();
+                    sequence.add(A[i]);
+
+                    // Detect direction
+                    direction = detectDirection(A, i);
+                }
+
+                
+            }
+        }
+
+        // Trim sequences of repeats at both ends
+        for (ArrayList<Integer> seq : sequences) {
+            // Trim beginning
+            int index = 0;
+            while (seq.get(index) == seq.get(index+1)) {
+                seq.remove(index);
+            }
+
+            // Trim ending
+            index = seq.size()-1; //last item
+            while (seq.get(index) == seq.get(index-1)) {
+                seq.remove(index);
+            }
+        }
+        
+        
+        
+        int seqSize = 0;
+        if (sequences.size() == 1) {
+            seqSize = sequences.get(0).size();
+            return seqSize;
+        }
+
+        // Select shortest sequence
+        ArrayList<Integer> minSeq = new ArrayList<Integer>();
+        for (int j = 0; j < sequences.size(); j++) {
+            if (minSeq.size() == 0 || minSeq.size() > sequences.get(j).size()) {
+                minSeq = sequences.get(j);
+            }
+        }
+
+        return minSeq.size();
+    }
+
+    public static int detectDirection (int[] A, int index) {
+        int direction = 0;
+        for (int j = 0; j < A.length; j++) {
+            if (A[j] > A[index]) {
+                direction = 1;
+                break;
+            } else if (A[j] < A[index]) {
+                direction = 0;
+                break;
+            }
+        }
+        return direction;
+    }
+
+    public static int allNumsWithinx(int[] A, int k) {
+        // Compute minimum length of a section of A that contains all the values from 0 to k-1.
+        // If such part does not exist, return 0
+
+        // Loop through items
+        int prev = 0;
+
+        ArrayList<ArrayList<Integer>> rows = new ArrayList<ArrayList<Integer>>();
+
+        ArrayList<Integer> row = new ArrayList<Integer>();
+        int direction = 0; // 0 for discending, 1 for ascending
+        for (int i = 0; i < A.length; i++) {
+
+
+            if (i != 0) {
+                if (direction == 0 && A[i] < prev) {
+                    // Descending order
+                    row.add(A[i]);
+                } else if ( direction == 1 && A[i] > prev) {
+                    // Ascending order
+                    row.add(A[i]);
+                } else {
+                    // Direction change
+                    
+                    // Check if row meets conditions
+
+
+                    rows.add(row);
+                    
+                    // Reset variables
+                    row.clear();
+                    direction = 0;
+
+                }
+            } else {
+                prev = A[i];
+            }
+        
+            
+            
+        }
+
         return 0;
     }
 
