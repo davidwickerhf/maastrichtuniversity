1f0af947adcb270499d835d0498b96e18c342cfb
David Wicker <davidwickerhf@gmail.com>
2023-09-26 14:32:06 +0200

    Add plausible analytics

--- notes/quartz/components/Head.tsx
+++ notes/quartz/components/Head.tsx
@@ -18,6 +18,7 @@ export default (() => {
     return (
       <head>
         <title>{title}</title>
+        <script defer data-domain="notes.wicker.life" src="https://plausible.io/js/script.js"></script>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
